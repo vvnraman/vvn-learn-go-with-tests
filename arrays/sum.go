e27b20913601910c4cef81a72a01202dddd56682
Prateek Raman <vvnraman@gmail.com>
2020-06-06 19:56:07 -0400

    Arrays and slices - Sat Jun  6 19:56:07 EDT 2020

---
+++ arrays/sum.go
@@ -0,0 +1,31 @@
+package main
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if 0 == len(numbers) {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return sums
+}
