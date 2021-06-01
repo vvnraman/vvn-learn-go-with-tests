16e20db3ec68c9a029b78fe00ad864f0c303bca6
Prateek Raman <vvnraman@gmail.com>
2021-06-01 09:21:30 -0400

    Reflection in progress Tue Jun  1 09:21:30 EDT 2021

--- src/reflection/reflection.go
+++ src/reflection/reflection.go
@@ -1,9 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
-func walk(x interface{}, fn func(input string)) {
+func walk_v1(x interface{}, fn func(input string)) {
 	fn("Germany beat Brazil 7-0")
+}
+
+func walk_v2(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+
+	// this would panic if the struct has no fields in it.
+	field := val.Field(0)
+
+	// this would be wrong if the type of the field is not string
+	fn(field.String())
+}
+
+func walk_v3(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
+
 }
 
 func main() {
--- src/reflection/reflection_test.go
+++ src/reflection/reflection_test.go
@@ -1,23 +1,86 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestWalk(t *testing.T) {
+func TestWalk_V1(t *testing.T) {
+
+	// Test for a function `walk` which takes any struct and a method, and calls
+	// all the method passing in all the `string` fields of the struct.
 
 	expected := "Chris"
-	var got []string
+	var got []string // slice of strings
 
-	x := struct {
+	x := struct { // anonymous struct
 		Name string
 	}{expected}
 
-	walk(x, func(input string) {
+	walk_v1(x, func(input string) { // anonymous function
 		got = append(got, input)
 	})
 
 	if len(got) != 1 {
 		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	}
+}
+
+func TestWalk_V2(t *testing.T) {
+
+	// Test for a function `walk` which takes any struct and a method, and calls
+	// all the method passing in all the `string` fields of the struct.
+
+	expected := "Chris"
+	var got []string // slice of strings
+
+	x := struct { // anonymous struct
+		Name string
+	}{expected}
+
+	walk_v2(x, func(input string) { // anonymous function
+		got = append(got, input)
+	})
+
+	if got[0] != expected {
+		t.Errorf("got %q want %q", got[0], expected)
+	}
+}
+
+func TestWalk_v3(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"Struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		{
+			"Struct with two string fields",
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk_v3(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
